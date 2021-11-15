b5e5ad6695d486386107c06aea97df9bc9443bde
Tkachenko_Ann <summer.199@mail.ru>
2021-11-15 10:19:33 +0300

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import './App.css';
 import AppRouter from "./components/AppRouter";
 import NavBar from "./components/NavBar";
 import {Layout} from "antd";
+import {useActions} from "./hooks/useActions";
+import {IUser} from "./models/user";
 
 const App: FC = () => {
-  return (
-      <Layout>
-          <NavBar/>
-          <Layout.Content>
-              <AppRouter/>
-          </Layout.Content>
-      </Layout>
-  );
+    const {setUser, setIsAuth} = useActions();
+    useEffect(() => {
+        if (localStorage.getItem('auth')) {
+            setUser({username: localStorage.getItem('username' || '')} as IUser);
+            setIsAuth(true);
+        }
+    })
+      return (
+          <Layout>
+              <NavBar/>
+              <Layout.Content>
+                  <AppRouter/>
+              </Layout.Content>
+          </Layout>
+      );
 }
 
 export default App;
